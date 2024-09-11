86914cd27cdcabf3f3ee0d19b6029a5a2fc56913
sjlee7373 <sjlee7373@gmail.com>
2024-09-11 16:45:06 +0900

    Update simul.java
    test code

--- simul.java
+++ simul.java
@@ -17,9 +17,7 @@ public class SimpleCalculator {
 
         // 결과 출력
         System.out.println("두 숫자의 합은: " + sum); 
-while(true){  
-         ; 
-} 
+
         // 스캐너 종료
         // scanner.close(); 
    
